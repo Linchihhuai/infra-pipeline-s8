6702b45702d07eb74a146a2de8ea8b2df30214f8
Chih Huai Lin <chihhuai.lin@student.vinci.be>
2021-11-15 09:42:20 +0100

    pipeline change test

--- views/accueil.php
+++ views/accueil.php
@@ -1,5 +1,5 @@
 		<section id="contenu">
 			<h2>Accueil</h2>
-			<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP deployé sur Heroku!!!</p>
+			<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP deployé sur Heroku!?</p>
 			<p>Jean-Luc Collinet.</p>
 		</section>
