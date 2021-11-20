61bf04196c366b3d1ebc18913a6f49e5b2b77781
Chih Huai Lin <chihhuai.lin@student.vinci.be>
2021-11-20 17:25:23 +0100

    Test CI

--- views/accueil.php
+++ views/accueil.php
@@ -1,5 +1,5 @@
 		<section id="contenu">
 			<h2>Accueil</h2>
-			<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP deployé sur Heroku!?</p>
+			<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP deployé sur Heroku. Now we have CI!</p>
 			<p>Jean-Luc Collinet.</p>
 		</section>
