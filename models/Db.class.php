cf40f56a65178a8e6bb741f964b4e5f56bbac94a
Chih Huai Lin <chihhuai.lin@student.vinci.be>
2021-11-20 15:43:49 +0100

    modify connection lines

--- models/Db.class.php
+++ models/Db.class.php
@@ -7,8 +7,11 @@ class Db
     private function __construct()
     {
         try {
-            $this->_db = new PDO('mysql:host=localhost;dbname=bdbn;charset=utf8', 'root', '');
-            $this->_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            // $this->_db = new PDO('mysql:host=localhost;dbname=bdbn;charset=utf8', 'root', '');
+            // $this->_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+            $db = parse_url(getenv("postgres://rtmhswkqzudkyy:ca9e5a997be515c91c5c40260f98a8b2fca67e6b3c0338dd547a4f96e30cfe1f@ec2-54-228-139-34.eu-west-1.compute.amazonaws.com:5432/d6tfqmsrcf8kk3"));
+			$db["path"] = ltrim($db["path"], "/");
         } 
 		catch (PDOException $e) {
 		    die('Erreur de connexion à la base de données : '.$e->getMessage());
